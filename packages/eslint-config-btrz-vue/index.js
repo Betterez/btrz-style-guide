c8af5f03a9ef331b01ac7ed5199cc23aebae9bf1
Hernan Garcia <hernan@dynamicprogrammer.com>
2026-06-26 16:52:35 -0400

    [HG] update eslint for vue

--- packages/eslint-config-btrz-vue/index.js
+++ packages/eslint-config-btrz-vue/index.js
@@ -1,23 +1,37 @@
+const {FlatCompat} = require("@eslint/eslintrc");
+const btrzBase = require("eslint-config-btrz-base");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
 module.exports = {
-  extends: [
-    "eslint-config-btrz-base",
-    "plugin:vue/essential"],
-  parserOptions: {  // copied from airbnb-config-airbnb-base (see README)
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  settings: {
-    'import/resolver': {
-      nuxt: {
-        extensions: ['.vue']
-      }
-    },
-  },
-  plugins: [
-    "html"
-  ],
-  "env": {
-    "browser": true,
-    "jquery": true
+  configs: {
+    all: [
+      btrzBase.configs.all,
+      ...compat.config({
+        extends: [
+          "plugin:vue/essential"
+        ],
+        parserOptions: { // copied from airbnb-config-airbnb-base (see README)
+          ecmaVersion: 2018,
+          sourceType: "module",
+        },
+        settings: {
+          "import/resolver": {
+            nuxt: {
+              extensions: [".vue"]
+            }
+          },
+        },
+        plugins: [
+          "html"
+        ],
+        env: {
+          browser: true,
+          jquery: true
+        }
+      })
+    ]
   }
 };
